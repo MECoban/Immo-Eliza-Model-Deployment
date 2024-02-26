fbb05e61dfdf52fd11ea8ce9dd4d5fa7807e8c95
Léa <konincks.lea@gmail.com>
2024-02-26 14:35:50 +0100

    i think working predict and model. reqdy for the API but gonna perform some tests on it

---
+++ src/predict.py
@@ -0,0 +1,46 @@
+import joblib
+import pandas as pd
+
+class Predictor:
+
+
+    def __init__(self, num_predict, fl_predict, cat_predict, data, prediction):
+
+        self.num_predict = num_predict
+        self.fl_predict = fl_predict
+        self.cat_predict = cat_predict
+
+        self.data = pd.DataFrames()
+        self.prediction = pd.DataFrames()
+
+
+    def take (self, path):
+
+        self.data = pd.read_json(path)
+
+
+    def give (self, prediciton):
+
+        return prediction.to_json()
+
+
+    def predict (self, data, prediction):
+
+        model_name = 'LinearRegression'
+    
+        artifacts = joblib.load(f"models/{model_name}.joblib")
+    
+        # Unpack the artifacts
+        num_features = artifacts["features"]["num_features"]
+        fl_features = artifacts["features"]["fl_features"]
+        cat_features = artifacts["features"]["cat_features"]
+        imputer = artifacts["imputer"]
+        enc = artifacts["enc"]
+        model = artifacts["model"]
+    
+        # Extract the used data
+        data = data[num_features + fl_features + cat_features]
+    
+    
+        # Make predictions
+        prediction = model.predict(data[num_predict + fl_predict + cat_predict])
